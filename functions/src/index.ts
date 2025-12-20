a248a7d2f13698ae0a735aff6b334977a43c19a2
Mike Katholnig <mk3819@gmail.com>
2025-12-20 22:28:38 +0000

    chore: set callable region on submitIntake

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,8 @@
 import { initializeApp } from 'firebase-admin/app';
-import { onCall, HttpsError, setGlobalOptions } from 'firebase-functions/v2/https';
+import { onCall, HttpsError } from 'firebase-functions/v2/https';
 
 initializeApp();
-setGlobalOptions({ region: 'europe-west2' });
 
-export const submitIntake = onCall((request) => {
-  throw new HttpsError('unimplemented', 'Not implemented yet');
+export const submitIntake = onCall({ region: 'europe-west2' }, async (request) => {
+  throw new HttpsError('unimplemented', 'submitIntake not implemented yet');
 });
